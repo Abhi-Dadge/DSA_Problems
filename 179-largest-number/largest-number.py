a94e969ade4f4ed47bd9f9245cffe8b9b0d3cbf5
Abhishek Dadge <152360054+Abhi-Dadge@users.noreply.github.com>
2026-07-25 17:06:03 +0530

    Time: 9 ms (32.15%) | Memory: 12.3 MB (83.63%) - LeetSync

--- 179-largest-number/largest-number.py
+++ 179-largest-number/largest-number.py
@@ -7,7 +7,7 @@ class Solution(object):
         nums = list(map(str,nums))
         for i in range(len(nums)):
             for j in range(i+1,len(nums)):
-                if nums[i]+nums[j] < nums[j]+nums[i]:
-                    nums[i],nums[j] = nums[j],nums[i]
+                if nums[i]+nums[j]<nums[j]+nums[i]:
+                    nums[i],nums[j]=nums[j],nums[i]
         ans = "".join(nums)
-        return "0" if ans[0] == "0" else ans        
+        return "0" if ans[0]=="0"else ans
