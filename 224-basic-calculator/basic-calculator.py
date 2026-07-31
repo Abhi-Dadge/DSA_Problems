9385f8ec618d23f737eebfa56aca69824d31ce24
Abhishek Dadge <152360054+Abhi-Dadge@users.noreply.github.com>
2026-07-31 22:06:56 +0530

    Time: 128 ms (25.46%) | Memory: 13.5 MB (40.39%) - LeetSync

---
+++ 224-basic-calculator/basic-calculator.py
@@ -0,0 +1,35 @@
+class Solution(object):
+    def calculate(self, s):
+        #pending...
+        res = 0
+        num = 0 
+        sign = 1
+        stack = []
+
+        for ch in s:
+            if ch.isdigit():
+                num = num * 10 + int(ch)
+            elif ch == '+':
+                res += sign * num
+                num = 0
+                sign = 1
+            elif ch == '-':
+                res += sign * num
+                num = 0
+                sign = -1
+            elif ch =='(':
+                stack.append(res)
+                stack.append(sign)
+                res = 0
+                sign = 1
+            elif ch == ')':
+                res += sign * num
+                num = 0
+                res *= stack.pop()
+                res += stack.pop()
+        res += sign * num
+        return res
+
+
+       
+        
